80b683ec97c413dc8663ce9eca4734e2c0c2b8b5
majidsaadati <majidsaadati021@gmail.com>
2023-10-12 21:10:10 +0330

    add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import "./index.css"
+const ashkallist = [
+  { id:1, shekl:"moraba", arz:130, ertefa:130, gheymat:100},
+  { id:2, shekl:"mostatil", arz:140, ertefa:160, gheymat:65},
+  { id:3, shekl:"moraba", arz:90, ertefa:90, gheymat:43},
+  { id:4, shekl:"moraba", arz:25, ertefa:25, gheymat:15},
+]
+
 export default function Ashkal(){
-  return "salam"
+  
+  return (<div className="bg-slate-100 w-full" > 
+    {"salam"}
+  </div>)
 }
